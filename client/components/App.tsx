e2f8b617c84fb3271fc3212e8416b87f0489391a
Ricky <65267289+ricky-huang-dev@users.noreply.github.com>
2023-11-17 10:46:13 +1300

    idk

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -2,13 +2,11 @@ import { useFruits } from '../hooks/useFruits.ts'
 import weatherapi from '../apis/weatherapi.ts'
 
 function App() {
-  const { data } = useFruits()
   weatherapi()
   return (
     <>
       <div className="app">
         <h1>Fullstack Boilerplate - with Fruits!</h1>
-        <ul>{data && data.map((fruit) => <li key={fruit}>{fruit}</li>)}</ul>
       </div>
     </>
   )
